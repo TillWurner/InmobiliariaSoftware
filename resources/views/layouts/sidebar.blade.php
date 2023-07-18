33e7af98962c625f876c812d42d5a669ccc1e039
Kevin Gustavo <kg.2k.pl@gmail.com>
2023-07-18 17:55:17 -0400

    fix

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -290,7 +290,7 @@ background-position: center center">
         const csrfToken = '{{ csrf_token() }}';
         import { io } from "socket.io-client";
     
-        const socket = io("http://127.0.0.1:3000/",{
+        const socket = io("http://35.199.108.5:3000/",{
             transports: ["websocket"],
         });
 
